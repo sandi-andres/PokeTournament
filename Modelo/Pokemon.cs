8a272c729a115a928ab6c73f5d1bad571e12565b
sandi-andres <98196556+sandi-andres@users.noreply.github.com>
2023-11-02 09:03:16 -0600

    Add files via upload

---
+++ Modelo/Pokemon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPorgramacionIV.Modelo
+{
+    public class Pokemon
+    {
+        public int IdPokemon { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Tipo { get; set; }
+        public int Salud { get; set; }
+        public int Ataque { get; set; }
+        public int Defensa { get; set; }
+        public int Velocidad { get; set; }
+        public byte[] Imagen { get; set; }
+    }
+    public class Movimiento
+    {
+        public int IdMovimiento { get; set; }
+        public string Nombre { get; set; }
+        public string Tipo { get; set; }
+        public int Potencia { get; set; }
+
+    }
+
+}
